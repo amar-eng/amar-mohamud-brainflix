bc30c641b07b9bf1375412e64982e7a64bc95590
amar-eng <am24mo@gmail.com>
2020-11-08 22:57:04 -0500

    main video stylying done

--- sprint-1/src/App.js
+++ sprint-1/src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <MainVid />
-      <section className ="app-section">
+      <MainVid />
+      {/* <section className ="app-section">
         <MainContent />
         <VideoList />
       </section> */}
